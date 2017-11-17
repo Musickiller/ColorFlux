d0543c6c7b5866349c70abf80894ad19dae062c1
Musickiller <musickiller@yandex.ru>
2017-11-17 23:17:28 +0300

    comments, cleanup, smallest fixes.
    Code is almost readable now!

--- ColorFlux.py
+++ ColorFlux.py
@@ -11,17 +11,23 @@ from numpy.random import random as rnd
 # 2. Place
 # 3. Draw
 
+# Created mainly coz I'm lazy. Can be used to generate random int color values (0 .. 255).
 class Color:
+	# returns a variable that has "all colors you may need". That or just white. Again, coz I'm lazy.
 	def CreateColorDict(self):
 		colorDict = {
 			"white" : [255, 255, 255]
 			}
 		return colorDict
 		
+	# returns a random integer value (0 .. 255)
 	def RndColVal(self):
 		return int(rnd()*256)
 		
-	def RandomColor(self, nodeNum):
+	# returns a list of random color values.
+	# Actually, a list of lists.
+	# Takes number of [RGB] lists to return as nodeNum parameter
+	def RandomColor(self, nodeNum = 1):
 			# Log("Generating random node colors...", 3)
 			ret = []
 			for i in range(nodeNum):
@@ -29,7 +35,7 @@ class Color:
 			return ret
 		
 		
-		
+# Creates a colored square (or a rect. does not care as long as you put in 4 dots) (what a surprise!)
 class ColSquare(Color):
 	def __init__(self):
 		# Log("A square has been created!", 2)
@@ -37,14 +43,15 @@ class ColSquare(Color):
 		self.vertLst = gfx.vertex_list_indexed(4, [0, 1, 2, 0, 2, 3], 'v2i', 'c3B')
 		self.changingColors = False
 
+	# adds vertices to a vertices list. Sort of tells what place to take.
+	# Takes a list of [X, Y] lists. (each 4 dots of a rectangle have to be listed)
 	def Place(self, dots):
 		# Log("Placing a square on the window..", 3)
 		for i in range(4):
-			# Log("i = " + str(i), 4)
-			# print(dots[i])
-			# print(self.vertLst.vertices[i*2:(i*2+2)])
 			self.vertLst.vertices[i*2:(i*2+2)] = dots[i]
 			
+	# Can paint the whole rectangle in %color%
+	# color is a string name from colotDict dictionary
 	def PaintWhole(self, color):
 		# Log("Painting the whole square..", 3)
 		try:
@@ -55,6 +62,7 @@ class ColSquare(Color):
 			# Log("i = " + str(i), 4)
 			self.vertLst.colors[i*3:(i*3+3)] = colVal
 			
+	# Draws rectangle and refreshes it's colors if they have to be refreshed.
 	def Draw(self):
 		# Log("Drawing! ^_^", 3) #< bad idea to print to console anything at 60 FPS
 		self.vertLst.draw(pyglet.gl.GL_TRIANGLES)
@@ -63,27 +71,20 @@ class ColSquare(Color):
 			self.colFlux.Change()
 			colVal = self.colFlux.GetColors()
 			for i in range(4):
-				# Log("i = " + str(i), 4)
-				# print("!!!!!!!")
-				# print(colVal)
-				# print(self.vertLst.colors[i*3:(i*3+3)])
 				self.vertLst.colors[i*3:(i*3+3)] = colVal[i]
 		else:
 			Log("Static color", 2)
-		
+	# The same as PaintWhole just so much more fun.
 	def PaintFlux(self):
 		# Log("Let the FUN begin!!", 3)
 		self.changingColors = True
 		self.colFlux = ChangingColors(4)
 		colVal = self.colFlux.GetColors()
 		for i in range(4):
-			# Log("i = " + str(i), 4)
-			# print("!!!!!!!")
-			# print(colVal[i])
-			# print(self.vertLst.colors[i*3:(i*3+3)])
 			self.vertLst.colors[i*3:(i*3+3)] = colVal[i]
 
-		
+# I should someday remake it to become a parent class. Really.
+# Creates an object that can manipulate colors and stores color values. Handy!
 class ChangingColors(Color):
 	def __init__(self, nodeNum):
 		# Log("ChangingColors object is being created..", 2)
@@ -100,10 +101,12 @@ class ChangingColors(Color):
 				a += [int(rnd()*maxSpdChg+minSpdChg)]
 			self.nodColChgSpd += [a]
 		# Log("done!", 2)
-	
+		
+	# Gives colors!
 	def GetColors(self):
 		return self.nodeColors
 	
+	# Fluidly changes colors
 	def Change(self):
 		# change color for each node according to speed
 		# Log("Changing colors...", 3)
@@ -125,34 +128,26 @@ class ChangingColors(Color):
 	
 
 
-# Graphics class for batched graphics
-# class Graphics:
-	# self.objects = []
-	# self.objectCount = 0
-	
-	# def __init__(self):
-		# self.batch = gfx.Batch()
-	
-	# def Draw():
-		# self.batch.draw()
-		
-	# def AddColSquare():
-		# self.objects += []
+
 		
 # RiseLogLevel(1)
 
+# Creates the window
 window = pyglet.window.Window()
 FPS = 60
 figures = [False]
 
+# Ugh.. almost unused
 canvSizeX = 640
 canvSizeY = 480
 
+# Just to make it easier to reshape a rectangle
 A = [0, 0]
 B = [0, canvSizeY]
 C = [canvSizeX, canvSizeY]
 D = [canvSizeX, 0]
 
+# called each time smth is drawn. Should use something else here, possibly...
 @window.event
 def on_draw():
 	if figures[0] == False:
@@ -164,6 +159,7 @@ def on_draw():
 		figures[0] = sq
 		sq.Draw()
 
+# Used to change frames.
 def Frame(dt):
 	# Log("\n\n\nNEW FRAME!!!\n\n\n")
 	sq = figures[0]
@@ -171,7 +167,8 @@ def Frame(dt):
 		window.clear()
 		sq.Draw()
 	
+# calls Frame each 1/FPS seconds
 pyglet.clock.schedule_interval(Frame, 1/FPS)
 
-
+# where would we be without this one?
 pyglet.app.run()
